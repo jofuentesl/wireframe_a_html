6a5eb66a40c16def3ea9172408027abd27002eef
jofuentesle <jfuentesleiva@gmail.com>
2021-01-15 18:58:35 +0100

    updata header

--- index.php
+++ index.php
@@ -5,13 +5,28 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" contenet="Exercici HTML curs itAcademy itinerari Angular">
     <meta name="keywords" content="html5, Angular, itAcademy">
-    <link rel="stylesheet" href="styles/main.css" type="text/css" media="screen"> 
+    <link rel="stylesheet" href="styles/main.css" type="text/css" media="screen">
+    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
     <title>Exercici HTML mòdul 2</title>
 </head>
 <body>
     <?php
     include 'layouts/header.php';
     ?>
+    <nav class="nav__main">
+        <ul class="block__nav__main">
+            <li class="block__nav__item"><a href="#">menú 1</a></li>
+            <li class="block__nav__item"><a href="#">menú 2</a></li>
+            <li class="block__nav__item"><a href="#">menú 3</a></li>
+            <li class="block__nav__item"><a href="#">menú 4</a></li>
+            <li class="block__nav__item"><a href="#">menú 5</a></li>
+        </ul>
+    </nav>
+    <section class="block__1">
+        <article class="block__article">hola</article>
+        <article class="block__article">hola</article>
+        <article class="block__article">hola</article>
+    </section>
     main
     <?php
         include 'layouts/footer.php';
--- layouts/header.php
+++ layouts/header.php
@@ -1,3 +1,10 @@
-<header> 
-    header works
+<header class="block__header"> 
+    <img class="block__header__logo" src="./images/header/logo.svg" alt="logo barnaBit">
+    <nav class="block__nav">
+        <ul class="block__nav__rrss">
+            <li class="rrss__nav__item"><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li class="rrss__nav__item"><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li class="rrss__nav__item"><a href="#"><i class="fa fa-rss"></i></a></li>
+        </ul>
+    </nav>
 </header>
